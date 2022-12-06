43a71d562f49bf12b4efc616a1f6d626c0c5b304
Muthanuni Eversmille <muthanunie@gmail.com>
2022-12-06 22:00:42 +0200

    initial commit ES-6 Eve-Admin: Set up Eve-store admin mvc application

---
+++ eve-shop-admin/src/main/java/com/evecoding/eveshopadmin/EveShopAdminApplication.java
@@ -0,0 +1,13 @@
+package com.evecoding.eveshopadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveShopAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveShopAdminApplication.class, args);
+    }
+
+}
---
+++ eve-shop-admin/src/test/java/com/evecoding/eveshopadmin/EveShopAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evecoding.eveshopadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveShopAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
